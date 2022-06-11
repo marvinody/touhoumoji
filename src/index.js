ea453ced9cdd68d13ab1e30066018623f96691a6
Manuel Bugallo <marvinody@gmail.com>
2022-06-11 18:38:42 -0400

    adds more character and some extra styling

--- src/index.js
+++ src/index.js
@@ -53,6 +53,25 @@ const toggle = function (elem) {
 
 };
 // end stolen
+// stolen from https://stackoverflow.com/questions/2450954
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
 const CE = (t) => document.createElement(t)
 
 const getCharacters = (cb) => {
@@ -81,9 +100,9 @@ const findParentMatchingClass = (el, wantedClass) => {
 }
 
 const addCharacters = (data) => {
-  console.log(data)
 
-
+  shuffle(data)
+  
   const entries = data.map(char => {
     const emote = CE('div')
     emote.classList.add('emote')
